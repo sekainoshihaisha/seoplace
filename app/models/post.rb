b0ef14e37094fe2003e437cc66f3a1a5a72399b6
Brian Tellez <brianulises0197@gmail.com>
2017-01-02 03:59:27 +0000

    Addedd CKeditor and hyperlink parser

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,18 @@
 class Post < ActiveRecord::Base
+    before_save :clean_links
+    
+    validates_presence_of :title
+    validates_presence_of :body
+    
+    def clean_links
+        # We use DocumentFragment so Nokogiri doesn't add DOCTYPE and other HTML
+        doc = Nokogiri::HTML::DocumentFragment.parse(body)
+        
+        doc.css('a').each do |a|
+            a[:rel] = 'nofollow'
+            a[:target] = '_blank'
+        end
+        
+        self.body = doc.to_s
+    end
 end
