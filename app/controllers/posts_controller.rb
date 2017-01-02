213ba7849f3bd624b7c2d6b9d8afefbc9913c379
Brian Tellez <brianulises0197@gmail.com>
2017-01-02 01:41:40 +0000

    Title tag

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,11 +5,13 @@ class PostsController < ApplicationController
   # GET /posts.json
   def index
     @posts = Post.all
+    @meta_title = meta_title 'The place to find SEO tips'
   end
 
   # GET /posts/1
   # GET /posts/1.json
   def show
+    @meta_title = meta_title @post.title
   end
 
   # GET /posts/new
